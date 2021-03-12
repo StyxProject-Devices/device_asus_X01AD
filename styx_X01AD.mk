be2c9f571d6442972d25f0daf0372e80b2990ed0
AP-XD <arkamtg.pramanik@gmail.com>
2021-03-12 23:59:05 +0530

    X01AD: Initial adaptation for Styx OS
    Add Smart charge
    Address Smart charge sepolicy denials
    Add background blur
    Disable VOIP compression in Board_Config.mk
    
    Signed-off-by: AP-XD <arkamtg.pramanik@gmail.com>

---
+++ styx_X01AD.mk
@@ -0,0 +1,50 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/asus/X01AD
+
+# Inherit from source
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Set shipping API level
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
+# Inherit common Pixelexperience stuff.
+$(call inherit-product, vendor/styx/config/common.mk)
+TARGET_BOOT_ANIMATION_RES := 720
+TARGET_GAPPS_ARCH := arm64
+
+# Pixel customization
+TARGET_INCLUDE_STOCK_ARCORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+
+# Inherit device stuff
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
+
+# Device identifiers.
+PRODUCT_DEVICE := X01AD
+PRODUCT_NAME := styx_X01AD
+PRODUCT_BRAND := asus
+PRODUCT_MODEL := ASUS_X01AD
+PRODUCT_MANUFACTURER := asus
+TARGET_VENDOR := asus
+BOARD_VENDOR := asus
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="msm8953_64-user 10 WW_Phone-202005071625 17.2018.2004.31-20200507 release-keys"
+BUILD_FINGERPRINT := asus/WW_X01AD/ASUS_X01A_1:10/WW_Phone-202005071625/17.2018.2004.31-20200507:user/release-keys
+
+PRODUCT_GMS_CLIENTID_BASE := android-asus
