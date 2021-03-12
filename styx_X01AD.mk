032abdd43e9cffc6b3e05d17a066f48eb0955598
AP-XD <arkamtg.pramanik@gmail.com>
2021-03-12 23:59:05 +0530

    X01AD: Initial adaptation for Styx OS
    Add Smart charge
    Address Smart charge sepolicy denials
    Add background blur
    Disable VOIP compression in Board_Config.mk
    
    Signed-off-by: AP-XD <arkamtg.pramanik@gmail.com>

--- styx_X01AD.mk
+++ styx_X01AD.mk
@@ -1,7 +1,17 @@
 #
-# Copyright (C) 2020 The PixelExperience Project
+# Copyright (C) 2019 The LineageOS Project
 #
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 DEVICE_PATH := device/asus/X01AD
@@ -14,7 +24,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit common Pixelexperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/styx/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 720
 TARGET_GAPPS_ARCH := arm64
 
